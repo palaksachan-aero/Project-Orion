4dd50903c01c612435a15b825447164e2bd22b2a
Palak Sachan <1palaksachan@gmail.com>
2026-07-08 20:18:55 +0530

    Add first Python program

---
+++ Week-01/hello_aerospace.py
@@ -0,0 +1,13 @@
+print("=" * 40)
+print("🚀 Welcome to Project Orion")
+print("=" * 40)
+
+name = "Palak Sachan"
+branch = "Aerospace Engineering"
+goal = "Become an Aerospace Researcher"
+
+print(f"Name   : {name}")
+print(f"Branch : {branch}")
+print(f"Goal   : {goal}")
+
+print("\nThis is my first Python program!")
